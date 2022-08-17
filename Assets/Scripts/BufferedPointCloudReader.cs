269b9084e98d5ba03a8b0e95df3eb829b278440d
Christopher Remde <christopher.remde@outlook.de>
2022-08-17 17:00:57 +0200

    Fixed Bug that could appear when non .ply files where in Data directory

--- Assets/Scripts/BufferedPointCloudReader.cs
+++ Assets/Scripts/BufferedPointCloudReader.cs
@@ -7,186 +7,194 @@ using System.Globalization;
 
 public struct Frame
 {
-	public int frameNumber;
+    public int frameNumber;
     //public float time;
-	public bool frameDataIsAvailable;
-	public FrameData frameData;
+    public bool frameDataIsAvailable;
+    public FrameData frameData;
 }
 
 public struct FrameData
 {
-	public Vector3[] frameVertices;
-	public Color[] frameColors;
+    public Vector3[] frameVertices;
+    public Color[] frameColors;
 }
 
 public class BufferedPointCloudReader
 {
     public string folder;
-	public int nFrames = 0;
-	public int nFramesRead = 0;
-	public float timeOffset = 0.0f;
+    public int nFrames = 0;
+    public int nFramesRead = 0;
+    public float timeOffset = 0.0f;
     public Frame[] frameBuffer;
-	private String[] configFileLines;
-	public string[] plyFilePaths;
+    private String[] configFileLines;
+    public string[] plyFilePaths;
 
-	public BufferedPointCloudReader(string folder)
+    public BufferedPointCloudReader(string folder)
     {
-		this.folder = folder;
+        this.folder = folder;
 
-		try
+        try
         {
-			plyFilePaths = Directory.GetFiles(folder, "*.ply");
-		}
+            plyFilePaths = Directory.GetFiles(folder, "*.ply");
+        }
 
-		catch(Exception e)
+        catch (Exception e)
         {
-			Debug.LogError("Pointcloud path is not valid!");
-			return;
-		}
+            Debug.LogError("Pointcloud path is not valid!");
+            return;
+        }
 
-		if (plyFilePaths.Length == 0)
-			Debug.LogError("Could not find any ply files!");
-	}
+        if (plyFilePaths.Length == 0)
+            Debug.LogError("Could not find any ply files!");
+    }
 
     public void ReadFrame()
-	{
-		if (nFramesRead < nFrames && frameBuffer [nFramesRead].frameDataIsAvailable == false)
+    {
+        Debug.Log("Nframes = " + nFrames + " nFramesRead = " + nFramesRead + ", frameBuffer = " + frameBuffer.Length);
+
+        if (nFramesRead < nFrames)
         {
-            BinaryReader reader = new BinaryReader(new FileStream(plyFilePaths[nFramesRead], FileMode.Open));
 
-			bool doubleUsed = false;
-			bool alphaUsed = false;
-
-			string line = "";
-
-			while (!line.Contains("element vertex"))
-			{
-				line = readHeaderLine(reader);
-			}
-
-			string[] lineElements = line.Split(' ');
-
-			int numberOfPointsInFrame = Int32.Parse(lineElements[2]);
-
-			line = readHeaderLine(reader);
-
-			if (line.Contains("double"))
-				doubleUsed = true;
-			else if (line.Contains("float"))
-				doubleUsed = false;
-
-			while (!line.Contains("blue"))
-			{
-				line = readHeaderLine(reader);
-			}
-
-			line = readHeaderLine(reader);
-
-			if(line.Contains("alpha"))
-				alphaUsed = true;
-
-			bool skipToEnd = true;
-			//Skip to the end of the header
-			if (line.Contains("end_header"))
+            if (frameBuffer[nFramesRead].frameDataIsAvailable == false)
             {
-				skipToEnd = false;
-			}
+                BinaryReader reader = new BinaryReader(new FileStream(plyFilePaths[nFramesRead], FileMode.Open));
 
-            if (skipToEnd)
-            {
-				while (!line.Contains("end_header"))
-				{
-					line = readHeaderLine(reader);
-				}
-			}
+                bool doubleUsed = false;
+                bool alphaUsed = false;
 
-			frameBuffer [nFramesRead].frameData.frameVertices = new Vector3[numberOfPointsInFrame];
-			frameBuffer [nFramesRead].frameData.frameColors = new Color[numberOfPointsInFrame];
+                string line = "";
 
-			float x, y, z;
-			byte r, g, b, a;
-
-            for (int i = 0; i < numberOfPointsInFrame; i++)
-            {
-                if (doubleUsed)
+                while (!line.Contains("element vertex"))
                 {
-					x = (float)reader.ReadDouble();
-					y = (float)reader.ReadDouble();
-					z = (float)reader.ReadDouble();
-				}
+                    line = readHeaderLine(reader);
+                }
 
-                else
+                string[] lineElements = line.Split(' ');
+
+                int numberOfPointsInFrame = Int32.Parse(lineElements[2]);
+
+                line = readHeaderLine(reader);
+
+                if (line.Contains("double"))
+                    doubleUsed = true;
+                else if (line.Contains("float"))
+                    doubleUsed = false;
+
+                while (!line.Contains("blue"))
                 {
-					x = reader.ReadSingle();
-					y = reader.ReadSingle();
-					z = reader.ReadSingle();
-				}
+                    line = readHeaderLine(reader);
+                }
+
+                line = readHeaderLine(reader);
+
+                if (line.Contains("alpha"))
+                    alphaUsed = true;
+
+                bool skipToEnd = true;
                 
+                //Skip to the end of the header
+                if (line.Contains("end_header"))
+                {
+                    skipToEnd = false;
+                }
 
-				frameBuffer [nFramesRead].frameData.frameVertices [i] = new Vector3(x,y,-z); // invert z axis from kinect data
+                if (skipToEnd)
+                {
+                    while (!line.Contains("end_header"))
+                    {
+                        line = readHeaderLine(reader);
+                    }
+                }
 
-				r = reader.ReadByte();
-				g = reader.ReadByte();
-				b = reader.ReadByte();
+                frameBuffer[nFramesRead].frameData.frameVertices = new Vector3[numberOfPointsInFrame];
+                frameBuffer[nFramesRead].frameData.frameColors = new Color[numberOfPointsInFrame];
 
-				if(alphaUsed)
-					a = reader.ReadByte();
+                float x, y, z;
+                byte r, g, b, a;
 
-				frameBuffer [nFramesRead].frameData.frameColors [i] = new Color((float)r / 256f, (float)g / 256f, (float)b / 256f, 1f);
+                for (int i = 0; i < numberOfPointsInFrame; i++)
+                {
+                    if (doubleUsed)
+                    {
+                        x = (float)reader.ReadDouble();
+                        y = (float)reader.ReadDouble();
+                        z = (float)reader.ReadDouble();
+                    }
+
+                    else
+                    {
+                        x = reader.ReadSingle();
+                        y = reader.ReadSingle();
+                        z = reader.ReadSingle();
+                    }
+
+
+                    frameBuffer[nFramesRead].frameData.frameVertices[i] = new Vector3(x, y, -z); // invert z axis from kinect data
+
+                    r = reader.ReadByte();
+                    g = reader.ReadByte();
+                    b = reader.ReadByte();
+
+                    if (alphaUsed)
+                        a = reader.ReadByte();
+
+                    frameBuffer[nFramesRead].frameData.frameColors[i] = new Color((float)r / 256f, (float)g / 256f, (float)b / 256f, 1f);
+                }
+
+                frameBuffer[nFramesRead].frameDataIsAvailable = true;
+
+                nFramesRead++;
+
+                reader.Close();
             }
-
-			frameBuffer [nFramesRead].frameDataIsAvailable = true;
-
-			nFramesRead++;
-
-            reader.Close();
         }
-	}
+    }
 
 
 
     public void ReadConfig()
     {
-		//configFileLines = File.ReadAllLines(folder + "_PointCloudConfig.txt");
+        //configFileLines = File.ReadAllLines(folder + "_PointCloudConfig.txt");
 
-		nFrames = Directory.GetFiles(folder).Length;
-		frameBuffer = new Frame[nFrames];
+        nFrames = Directory.GetFiles(folder, "*.ply").Length;
+        frameBuffer = new Frame[nFrames];
 
-		//timeOffset = Convert.ToSingle (configFileLines [0]);
+        //timeOffset = Convert.ToSingle (configFileLines [0]);
 
-		for (int i=1; i<nFrames; i++) // first line in file stores time offset
+        for (int i = 1; i < nFrames; i++) // first line in file stores time offset
         {
-			frameBuffer[i].frameDataIsAvailable = false;
-			//frameBuffer[i].time = Convert.ToSingle(configFileLines [i]);
+            frameBuffer[i].frameDataIsAvailable = false;
+            //frameBuffer[i].time = Convert.ToSingle(configFileLines [i]);
         }
 
     }
 
-	public void WriteConfig() {
-		configFileLines [0] = Convert.ToString (timeOffset);
-		File.WriteAllLines (folder + "_PointCloudConfig.txt", configFileLines);
-	}
-
-	private string currentFileName(int number)
-	{
-		string myString = number.ToString();
-		myString = myString.PadLeft(5, '0');
-		return myString;
-	}
-
-
-	private string readHeaderLine(BinaryReader reader)
+    public void WriteConfig()
     {
-		char currentChar = 'a';
-		string s = "";
+        configFileLines[0] = Convert.ToString(timeOffset);
+        File.WriteAllLines(folder + "_PointCloudConfig.txt", configFileLines);
+    }
 
-		while (currentChar !='\r' && currentChar != '\n')
+    private string currentFileName(int number)
+    {
+        string myString = number.ToString();
+        myString = myString.PadLeft(5, '0');
+        return myString;
+    }
+
+
+    private string readHeaderLine(BinaryReader reader)
+    {
+        char currentChar = 'a';
+        string s = "";
+
+        while (currentChar != '\r' && currentChar != '\n')
         {
-			currentChar = reader.ReadChar();
-			s += currentChar;
+            currentChar = reader.ReadChar();
+            s += currentChar;
         }
 
-		return s;
+        return s;
     }
 
 }
